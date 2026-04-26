cfd4893216615f7841dbbe07928ddce3aa015bdd
Chimpchi <chimpchistudios@gmail.com>
2026-04-26 13:06:21 -0500

    Updated Windows Build Scripts

--- build.bat
+++ build.bat
@@ -1,1 +1,13 @@
-MSBuild.exe NauiApp.sln -property:Configuration=Release
+set PROJECT=NauiApp
+
+set CONFIG=Debug
+if /I "%~1"=="-release" (
+    set CONFIG=Release
+    shift
+) else if /I "%~1"=="-r" (
+    set CONFIG=Release
+    shift
+)
+
+premake5 vs2022
+msbuild "%PROJECT%.sln" /p:Configuration=%CONFIG%
