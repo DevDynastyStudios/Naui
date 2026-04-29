87699dd6ce2a3782479228f619385ab8d924e616
Kiril Abadjiev <kikoabadjiev@gmail.com>
2026-04-29 21:37:20 +0300

    added stb_ds for the utils

--- naui/utils/list.h
+++ naui/utils/list.h
@@ -1,45 +1,13 @@
 #pragma once
 
-#include <stddef.h>
-
-typedef struct
-{
-    size_t len, cap;
-}
-Naui_ListHeader;
-
-#define NAUI_LIST_DEFAULT_CAP 64
-
-void *naui_list__init(size_t elem_size, size_t cap);
-void *naui_list__push(void *arr, size_t elem_size);
-void *naui_list__push_at(void *arr, size_t elem_size, size_t index);
-void  naui_list__pop(void *arr, size_t elem_size);
-void  naui_list__remove(void *arr, size_t elem_size, size_t index);
-void  naui_list__uremove(void *arr, size_t elem_size, size_t index);
-void *naui_list__reserve(void *arr, size_t elem_size, size_t len);
-void  naui_list__free(void *arr);
+#include <stb/stb_ds.h>
 
 #define Naui_List(type) type *
-
-#define naui_list_get_header(arr) ((Naui_ListHeader *)(arr) - 1)
-#define naui_list_len(arr)        (naui_list_get_header(arr)->len)
-
-#define naui_list_push(arr, data) do { \
-    arr = naui_list__push(arr, sizeof(*arr)); \
-    arr[naui_list_get_header(arr)->len - 1] = (data); \
-} while (0)
-
-#define naui_list_push_at(arr, data, index) do { \
-    arr = naui_list__push_at(arr, sizeof(*arr), (index)); \
-    arr[(index)] = (data); \
-} while (0)
-
-#define naui_list_pop(arr)              naui_list__pop(arr, sizeof(*arr))
-#define naui_list_remove(arr, index)    naui_list__remove(arr, sizeof(*arr), (index))
-#define naui_list_uremove(arr, index)   naui_list__uremove(arr, sizeof(*arr), (index))
-
-#define naui_list_reserve(arr, len) do { \
-    arr = naui_list__reserve(arr, sizeof(*arr), (len)); \
-} while (0)
-
-#define naui_list_free(arr) naui_list__free(arr)
+#define naui_list_length(arr)           arrlen(arr)
+#define naui_list_push(arr, data)       arrput(arr, data)
+#define naui_list_pop(arr)              arrpop(arr)
+#define naui_list_free(arr)             arrfree(arr)
+#define naui_list_reserve(arr, len)     arrsetcap(arr, len)
+#define naui_list_insert(arr, data, i)  arrins(arr, i, data)
+#define naui_list_remove(arr, i)        arrdel(arr, i)
+#define naui_list_uremove(arr, i)       arrdelswap(arr, i)
---
+++ naui/utils/map.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stb/stb_ds.h>
+
+#define Naui_StrMap(type)                   type *
+#define naui_strmap_get(map, key)           shget(map, key)
+#define naui_strmap_get_index(map, key)     shgeti(map, key)
+#define naui_strmap_put(map, key, value)    shput(map, key, value)
+#define naui_strmap_del(map, key)           shdel(map, key)
+#define naui_strmap_len(map)                shlen(map)
+#define naui_strmap_free(map)               shfree(map)
+#define naui_strmap_default(map, value)     shdefault(map, value)
+
+#define Naui_IntMap(type)                   type *
+#define naui_intmap_get(map, key)           hmget(map, key)
+#define naui_intmap_get_index(map, key)     hmgeti(map, key)
+#define naui_intmap_put(map, key, value)    hmput(map, key, value)
+#define naui_intmap_del(map, key)           hmdel(map, key)
+#define naui_intmap_len(map)                hmlen(map)
+#define naui_intmap_free(map)               hmfree(map)
+#define naui_intmap_default(map, value)     hmdefault(map, value)
