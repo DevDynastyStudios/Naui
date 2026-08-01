0935ee999c45ce158a929573f4fe0c68cc116911
boxDev2008 <kikoabadjiev@gmail.com>
2026-08-01 17:36:11 +0300

    removed old scripts

--- scripts/install_sokol_shdc.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-curl -O https://raw.githubusercontent.com/floooh/sokol-tools-bin/refs/heads/master/bin/win32/sokol-shdc.exe
-move /y sokol-shdc.exe ..
--- scripts/install_sokol_shdc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -O https://raw.githubusercontent.com/floooh/sokol-tools-bin/refs/heads/master/bin/linux/sokol-shdc
-mv sokol-shdc ../
-chmod +x ../sokol-shdc
